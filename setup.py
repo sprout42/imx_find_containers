a68a39cfec3c77bfd9eee6a387af9a27326b1637
Erin <493589+sprout42@users.noreply.github.com>
2020-09-14 13:00:06 -0400

    trying to get 'pip install -e' to work

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='find_containers',
+    packages=find_packages(),
+
+    entry_points={
+        'console_scripts': ['find_containers=find_container:main']
+    }
+)
