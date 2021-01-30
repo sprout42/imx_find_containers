9ec285fb645104bc7a54c685efa5f901e3f8026c
Erin <493589+sprout42@users.noreply.github.com>
2021-01-29 20:44:03 -0500

    moved PyYAML to optional

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+# optional YAML output requirements that can't quite be captured in 
+# a requirements.txt file:
+#   PyYAML>=5.4.1
+#
+# Also tested with:
+#   ruamel.yaml>=0.16.12
+
 setup(
     name='imx_find_containers',
     packages=find_packages(),
@@ -13,4 +20,7 @@ setup(
         'console_scripts': ['imx_find_containers=imx_find_containers:main']
     },
     install_requires=required,
+    extras_require={
+        "YAML":  ["PyYAML>=5.4.1"],
+    }
 )
