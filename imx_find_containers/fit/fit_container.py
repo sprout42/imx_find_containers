2d03e56a616db4b7eaff0abe3a9ff37e1951ea22
ac0rn <sprout42.blog@gmail.com>
2021-09-29 14:16:14 -0400

    fixed FIT is_container check to handle insufficient data sizes

--- imx_find_containers/fit/fit_container.py
+++ imx_find_containers/fit/fit_container.py
@@ -12,12 +12,16 @@ class FITContainer(Container):
     def is_container(cls, data, offset, verbose=False):
         # First 4 bytes will be the FDT_MAGIC of 0xD00DFEED
         # second 4 bytes is the size
-        magic, size = struct.unpack_from('>II', data[offset:])
-        return magic == 0xD00DFEED and len(data) >= size
+        if len(data) >= offset + 8:
+            magic, size = struct.unpack_from('>II', data[offset:])
+            print(hex(offset), hex(magic), hex(size))
+            return magic == 0xD00DFEED and len(data) >= size
+        else:
+            return False
 
     def init_from_data(self, data, offset):
-        # It isn't strictly necessary to parse the header here since the pyfdt 
-        # module will parse the entire FDT for us, but this will allow the scan 
+        # It isn't strictly necessary to parse the header here since the pyfdt
+        # module will parse the entire FDT for us, but this will allow the scan
         # results entry to be more meaningful
         self.hdr = FDTHeader(data, offset)
 
@@ -28,7 +32,7 @@ class FITContainer(Container):
         parsed_dtb = FdtBlobParse(io.BytesIO(dtb))
         dts = parsed_dtb.to_fdt().to_dts()
 
-        # Add the DTB and DTS contents as images with custom extensions to get 
+        # Add the DTB and DTS contents as images with custom extensions to get
         # exports of the files.
         self.images = [
             {'range': imgrange, 'fileext': f'dtb', 'data': dtb},
@@ -39,8 +43,8 @@ class FITContainer(Container):
         self.map_images_by_addr()
 
     def fix_offset(self, offset):
-        # FIT images can be in other images, so this function allows correcting 
-        # the offsets so they are correct according to the top-level file 
+        # FIT images can be in other images, so this function allows correcting
+        # the offsets so they are correct according to the top-level file
         # instead of the file they were extracted from.
         self.offset = offset
         self.end = offset + self.hdr.totalsize
